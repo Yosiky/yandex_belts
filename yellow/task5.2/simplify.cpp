898db7d4a0e440fb506626e34c11164fb4cba430
eestelle <yosiky@list.ru>
2024-02-29 16:10:51 +0300

    task5.2 simplify

--- yellow/task5.2/simplify.cpp
+++ yellow/task5.2/simplify.cpp
@@ -1,104 +1,89 @@
 #include <iostream>
 #include <string>
+#include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
+class Person {
+protected:
 
-class Student {
-public:
-
-    Student(string name, string favouriteSong) {
-        Name = name;
-        FavouriteSong = favouriteSong;
-    }
-
-    void Learn() {
-        cout << "Student: " << Name << " learns" << endl;
-    }
-
-    void Walk(string destination) {
-        cout << "Student: " << Name << " walks to: " << destination << endl;
-        cout << "Student: " << Name << " sings a song: " << FavouriteSong << endl;
-    }
-
-    void SingSong() {
-        cout << "Student: " << Name << " sings a song: " << FavouriteSong << endl;
-    }
+	const string Type;
+	const string Name;
 
 public:
-    string Name;
+
+	Person(const string &name, const string &type) : Name(name), Type(type) { }
+
+	virtual void Walk(const string &destination) const {
+        cout << getType() << ": " << getName() << " walks to: " << destination << endl;
+	}
+
+	const string &getName(void) const {
+		return (Name);
+	}
+
+	const string &getType(void) const {
+		return (Type);
+	}
+};
+
+class Student : public Person {
+public:
+
+    Student(const string &name, const string &favouriteSong)
+		: Person(name, "Student"), FavouriteSong(favouriteSong) { }
+
+    void Learn(void) const {
+        cout << getType() << ": " << getName() << " learns" << endl;
+    }
+
+    void Walk(const string &destination) const override {
+		Person::Walk(destination);
+		SingSong();
+    }
+
+    void SingSong(void) const {
+        cout << getType() << ": " << getName() << " sings a song: " << FavouriteSong << endl;
+    }
+
+private:
     string FavouriteSong;
 };
 
 
-class Teacher {
+class Teacher : public Person {
 public:
 
-    Teacher(string name, string subject) {
-        Name = name;
-        Subject = subject;
+    Teacher(const string &name, const string &subject)
+		: Person(name, "Teacher"), Subject(subject) { }
+
+    void Teach(void) const {
+        cout << getType() << ": " << getName() << " teaches: " << Subject << endl;
     }
 
-    void Teach() {
-        cout << "Teacher: " << Name << " teaches: " << Subject << endl;
-    }
-
-    void Walk(string destination) {
-        cout << "Teacher: " << Name << " walks to: " << destination << endl;
-    }
-
-public:
-    string Name;
+private:
     string Subject;
 };
 
 
-class Policeman {
+class Policeman : public Person {
 public:
-    Policeman(string name) {
-        Name = name;
-    }
 
-    void Check(Teacher t) {
-        cout << "Policeman: " << Name << " checks Teacher. Teacher's name is: " << t.Name << endl;
-    }
+    Policeman(const string &name) : Person(name, "Policeman") { }
 
-    void Check(Student s) {
-        cout << "Policeman: " << Name << " checks Student. Student's name is: " << s.Name << endl;
-    }
+	void Check(const Person &person) const {
+        cout << getType() << ": " << getName() << " checks " << person.getType() << ". " << person.getType() << "'s name is: " << person.getName() << endl;
+	}
 
-    void Check(Policeman p) {
-        cout << "Policeman: " << Name << " checks Policeman. Policeman's name is: " << p.Name << endl;
-    }
-
-    void Walk(string destination) {
-        cout << "Policeman: " << Name << " walks to: " << destination << endl;
-    }
-
-public:
-    string Name;
 };
 
-
-void VisitPlaces(Teacher t, vector<string> places) {
-    for (auto p : places) {
+void VisitPlaces(const Person &t, const vector<string> &places) {
+    for (const auto &p : places) {
         t.Walk(p);
     }
 }
-
-void VisitPlaces(Student s, vector<string> places) {
-    for (auto p : places) {
-        s.Walk(p);
-    }
-}
-
-void VisitPlaces(Policeman pol, vector<string> places) {
-    for (auto p : places) {
-        pol.Walk(p);
-    }
-}
-
 
 int main() {
     Teacher t("Jim", "Math");
